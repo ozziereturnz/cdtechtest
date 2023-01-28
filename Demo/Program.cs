6942edc3239029ad7f2a1acb85080e0e1d16c419
ozziereturnz <ozziereturnz@gmail.com>
2023-01-28 14:57:34 +1100

    Added console commands

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,79 @@
-﻿class Program
+﻿using RobotSim;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("TODO: Write code.");
-        Console.WriteLine("Press any key to exit.");
-        Console.ReadKey();
+        if (args.Length > 0) ProcessCommandsFromFile(args[0]);
+        else ProcessCommandsAsInput();
+    }
+
+    static void ProcessCommandsAsInput()
+    {
+        Robot robot = new Robot();
+        Console.WriteLine("Your robot is ready to receive commands.");
+        string command;
+        
+        do
+        {
+            Console.Write("> ");
+            command = Console.ReadLine();
+            ProcessCommand(robot, command);    
+        } while (!String.Equals(command, "EXIT", StringComparison.OrdinalIgnoreCase));
+    }
+
+    static void ProcessCommandsFromFile(string filename)
+    {
+
+    }
+
+    static void ProcessCommand(Robot robot, string input)
+    {
+        if (input is null) return;
+        
+        string command = input.Trim().ToUpper();
+        switch (command)
+        {
+            case "MOVE":
+            {
+                robot.Move();
+                break;
+            }
+            case "LEFT":
+            {
+                robot.Left();
+                break;
+            }
+            case "RIGHT":
+            {
+                robot.Right();
+                break;
+            }
+            case "REPORT":
+            {
+                robot.Report();
+                break;
+            }
+            case "HELP":
+            {
+                Console.WriteLine("Your robot can process the following commands: PLACE, MOVE, LEFT, RIGHT, REPORT, HELP, EXIT");
+                break;
+            }
+            default:
+            {
+                if (!command.StartsWith("PLACE ")) break; // Invalid command or EXIT, just ignore
+
+                string[] commandArgs = command.Length > 6 ? command.Substring(6).Split(',') : new string[0];
+                bool success = commandArgs.Length == 3;
+
+                success &= int.TryParse(commandArgs[0].Trim(), out int x);
+                success &= int.TryParse(commandArgs[1].Trim(), out int y);
+                success &= Enum.TryParse<Facing>(commandArgs[2].Trim(), out Facing facing);
+
+                if (success) robot.Place(x, y, facing);
+                else Console.WriteLine("Invalid use of PLACE command, use format PLACE x,y,facing");
+                break;
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # cdtechtest
 Technical test for Champion Data.
 
-Initial commits are primarily setting up empty files as I've never created a .net project using VS Code before, so I want to make sure that's all working before I write any real code.
+Initial commits are primarily setting up empty files as I've never created a .net project using VS Code before, so I want to make sure that's all working before I write any real code. This all took about 20 minutes.
+
+Unit test creation and implementation of Robot class took about an hour. This was mostly spent on tests as the implementation for each method is very simple.
