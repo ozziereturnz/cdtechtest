adc15285817122af316875a8b8cedb85f1d9077a
ozziereturnz <ozziereturnz@gmail.com>
2023-01-28 15:17:20 +1100

    Added file processing

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using RobotSim;
+﻿using System.IO;
+using RobotSim;
 
 class Program
 {
@@ -24,13 +25,26 @@ class Program
 
     static void ProcessCommandsFromFile(string filename)
     {
+        if (!File.Exists(filename))
+        {
+            Console.WriteLine("Provided filename does not exist.");
+            return;
+        }
 
+        Robot robot = new Robot();
+        foreach(string command in File.ReadLines(filename))
+        {
+            ProcessCommand(robot, command);
+        }
+
+        Console.WriteLine("File has been processed, press any key to exit.");
+        Console.ReadKey();
     }
 
     static void ProcessCommand(Robot robot, string input)
     {
         if (input is null) return;
-        
+
         string command = input.Trim().ToUpper();
         switch (command)
         {
@@ -66,9 +80,12 @@ class Program
                 string[] commandArgs = command.Length > 6 ? command.Substring(6).Split(',') : new string[0];
                 bool success = commandArgs.Length == 3;
 
-                success &= int.TryParse(commandArgs[0].Trim(), out int x);
-                success &= int.TryParse(commandArgs[1].Trim(), out int y);
-                success &= Enum.TryParse<Facing>(commandArgs[2].Trim(), out Facing facing);
+                int x = 0;
+                int y = 0;
+                Facing facing = 0;
+                if (success) success = int.TryParse(commandArgs[0].Trim(), out x);
+                if (success) success = int.TryParse(commandArgs[1].Trim(), out y);
+                if (success) success = Enum.TryParse<Facing>(commandArgs[2].Trim(), true, out facing);
 
                 if (success) robot.Place(x, y, facing);
                 else Console.WriteLine("Invalid use of PLACE command, use format PLACE x,y,facing");
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Technical test for Champion Data.
 Initial commits are primarily setting up empty files as I've never created a .net project using VS Code before, so I want to make sure that's all working before I write any real code. This all took about 20 minutes.
 
 Unit test creation and implementation of Robot class took about an hour. This was mostly spent on tests as the implementation for each method is very simple.
+
+Adding console commands took about 30 mins with interruptions. Adding file processing took about 15 mins.
