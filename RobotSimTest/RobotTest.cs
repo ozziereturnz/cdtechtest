fb6c64bc6e37fa19835ce882c21ab452083e2b1e
ozziereturnz <ozziereturnz@gmail.com>
2023-01-28 12:44:12 +1100

    Added empty test library

---
+++ RobotSimTest/RobotTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RobotSimTest
+{
+    [TestClass]
+    public class RobotTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // TODO: Write tests
+            Assert.IsTrue(true);
+        }
+    }
+}
