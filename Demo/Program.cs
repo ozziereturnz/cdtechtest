ace0bc7c95267220310990792cf7bfb4c03e6bba
ozziereturnz <ozziereturnz@gmail.com>
2023-01-28 12:38:48 +1100

    Added empty console app

---
+++ Demo/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("TODO: Write code.");
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
